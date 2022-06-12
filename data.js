1d3516f2a7bc4a0e191745126007c5d71dd864bb
zone202 <loveallunknown@gmail.com>
2022-06-12 19:31:16 -0400

    git init

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name : '7 Minutes',
+        path: 'music/Dean Lewis - 7 Minutes (Lyrics).mp3',
+        artist : 'Dean Lewis',
+        cover: 'Images/7_Minutes_by_Dean_Lewis.jpg'
+    },
+    {
+        name : 'Hurt',
+        path: 'music/Johnny Cash - Hurt.mp3',
+        artist : 'Johnny Cash',
+        cover: 'Images/Johnny Cash.jpg'
+    },
+    {
+        name : 'Photograph',
+        path: 'music/Ed Sheeran - Photograph (Lyrics).mp3',
+        artist : 'Ed Sheeran',
+        cover: 'Images/Photograph.jpg'
+    },
+]
